82d5a4c4aebea23babdbf09e3a096c0884d3eb99
Julian Bermudez <ja.bermudez10@uniandes.edu.co>
2026-05-25 17:26:15 -0500

    feat(actions): barrel export for all action functions

---
+++ src/actions/index.ts
@@ -0,0 +1,6 @@
+// src/actions/index.ts
+export * from "./portfolio.js"
+export * from "./assets.js"
+export * from "./trading.js"
+export * from "./history.js"
+export * from "./recommendations.js"
